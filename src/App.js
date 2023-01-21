347601f049f3996b5ab29f6f7e69dd890b5257e8
gotan-dancer <eugene.kalugin@gmail.com>
2023-01-21 19:45:53 +0500

    01 commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,81 @@
 import logo from './logo.svg';
 import './App.css';
 
+class App2 extends React.Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            curZone: {CurZone.curZone},
+        }
+    }
+
+    calcHours = () => {
+        let format = 'hh:mm:ss';
+
+        let m = moment();
+
+        const { curZone } = this.state.curZone;
+
+        return (m.add({h: curZone - Date.TimezoneOffset()}).format(format));
+    }
+
+    render() {
+        return (
+            <div>
+                <p>{this.calcHours()}</p>
+            </div>
+        )
+    }
+}
+
+function CapName(){
+    
+    return(
+        <div>
+            <table>
+                <tr rowspan="2">
+                    <h1>Название</h1>
+                    <input
+                        type="text"
+                        name="capName"
+                        defaultValue={this.state.capName}
+                        onChange={this.onChangeHandler}
+                    />
+                </tr>
+            </table>
+        </div>
+    )
+}
+
+function CurZone(){
+    
+    return(
+        <div>
+            <table>
+                <tr rowspan="2">
+                    <h1>Временная зона</h1>
+                    <input
+                        type="number"
+                        min="-12"
+                        max="12"
+                        name="curZone"
+                        defaultValue={this.state.curZone}
+                        onChange={this.onChangeHandler}
+                    />
+                </tr>
+            </table>
+        </div>
+    )
+}
+
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <CapName />
+        <CurZone />
+        <button caption="Добавить" onClick={ () => {return (<div><App2 /></div>)}}></button>
     </div>
   );
 }
